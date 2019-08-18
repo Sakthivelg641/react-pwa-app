82d4c93906871a015acaa7f0d3ac1f41b1bf5f7c
Sakthivelg641 <sakthivelg641@gmail.com>
2019-08-18 14:41:25 +0530

    added custom service worker

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Details from './components/profileCard';
 
+
 class App extends Component {
 
     // State to store the response from the API call
@@ -17,7 +18,9 @@ class App extends Component {
         .then(res => res.json())
         .then((data) => {
             this.setState({ profile: data.results});
+            localStorage.setItem('PROFILE', data.results);
         })
+
     }
 
     render() {
@@ -29,3 +32,37 @@ class App extends Component {
 }
 
 export default App;
+
+class PWAConfApp {
+  constructor() {
+    this.speakersDiv = document.querySelector('.speakers');
+    this.scheduleDiv = document.querySelector('.schedule');
+    this.init();
+  }
+  async init() {
+    await this.loadSpeakers();
+    this.loadSchedule();
+    this.registerSW();
+  }
+   async fetchJSON(url) {
+    const res = await fetch(url);
+    return res.json();
+  }
+  async registerSW() {
+    if ('serviceWorker' in navigator) {
+      try {
+        await navigator.serviceWorker.register('./customWorker.js');
+      } catch (e) {
+        alert('ServiceWorker registration failed. Sorry about that.');
+      }
+    } else {
+      document.querySelector('.alert').removeAttribute('hidden');
+    }
+  }
+}
+
+window.addEventListener('load', e => {
+  new PWAConfApp();
+});
+
+
